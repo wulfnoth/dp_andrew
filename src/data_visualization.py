ae78de041ecb4d157c3d48f3459e31295787a7dd
congj <congj.yang@hotmail.com>
2018-12-12 00:38:43 +0800

    gitignore

--- src/data_visualization.py
+++ src/data_visualization.py
@@ -2,28 +2,38 @@ import matplotlib.pyplot as plot
 import numpy as np
 import pandas as pd
 
-fig = plot.figure(1, figsize=(6, 4), dpi=100)
 
-ax = plot.subplot(111)
-ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')
-ax.spines['left'].set_position(('data', 0))
-ax.spines['bottom'].set_position(('data', 0))
-ax.set_xlim(-0.2, 1.2)
-ax.set_ylim(0, 1.2)
-ax.set_xticks([0, 1])
-ax.set_yticks([1])
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.set_xlabel(r'$x_1$')
-ax.set_ylabel(r'$x_2$')
+def pt(data, label, line):
+    plot.figure(1, figsize=(6, 4), dpi=100)
 
-data = pd.read_csv("../data/data.csv", dtype={'x1': np.float64, 'x2': np.float64, 'y': np.int32})
-nd_data = data.values
+    ax = plot.subplot(111)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data', 0))
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.set_xlim(-0.2, 1.2)
+    ax.set_ylim(0, 1.2)
+    ax.set_xticks([0, 1])
+    ax.set_yticks([1])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel(r'$x_1$')
+    ax.set_ylabel(r'$x_2$')
 
-colors = ['red', 'blue'] # red is negative, blue is positive
-for index, row in data.iterrows():
-    ax.scatter(row['x1'], row['x2'], color=colors[int(row['y'])])
+    # data = pd.read_csv("../data/data.csv", dtype={'x1': np.float64, 'x2': np.float64, 'y': np.int32})
+    # nd_data = data.values
 
-plot.show()
+    colors = ['red', 'blue']  # red is negative, blue is positive
+    shape = data.shape
+    x_lim = np.linspace(-0.2, 1.2)
+    print(line(x_lim))
+    ax.plot(x_lim, line(x_lim))
+    for i in range(shape[1]):
+        column = data[:, i]
+        # print(column[0])
+        ax.scatter(column[0], column[1], color=colors[int(label[0, i])])
+    # for index, row in data.iterrows():
+    #     ax.scatter(row['x1'], row['x2'], color=colors[int(row['y'])])
+
+    plot.show()
 
--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import data_visualization as dv
 
 
 class LogisticRegression:
@@ -7,15 +8,17 @@ class LogisticRegression:
     def __init__(self, dim_x):
         self.features_data = None
         self.labels = None
-        self.iter = 10
+        self.iter = 100
         self.b = np.zeros(shape=(1, 1))
-        self.alpha = 0.1
+        self.alpha = 0.01
         self.w = np.zeros(shape=(dim_x, 1))
+        self.m = 0
 
     def set_iter(self, iteration):
         self.iter = iteration
 
-    def transform(self, features_data, labels):
+    def fit(self, features_data, labels):
+        _, self.m = features_data.shape
         self.features_data = features_data
         self.labels = labels
         for i in range(self.iter):
@@ -23,24 +26,50 @@ class LogisticRegression:
 
     def lg(self):
         z = np.dot(self.w.T, self.features_data) + self.b
-        a = 1/(1+np.exp(-z))
-        dz = a-self.labels
-        dw = np.sum(self.features_data*dz, axis=1)
-        db = np.sum(dz, axis=1)
+        a = 1 / (1 + np.exp(-z))
+        dz = a - self.labels
+        # print(self.features_data.shape)
+        dw = np.sum(self.features_data * dz, axis=1, keepdims=True) / self.m
+        # print(dw)
+        db = np.sum(dz, axis=1) / self.m
 
-        self.w -= self.alpha*dw
-        self.b -= self.alpha*db
+        self.w -= self.alpha * dw
+        self.b -= self.alpha * db
 
     def print(self):
-        print("w is: \n\t", self.w)
-        print("b is: \n\t", self.b)
+        print("w is: \n\t", self.w / self.w[1, 0])
+        print("b is: \n\t", self.b / self.w[1, 0])
+
+
+global wr
+global br
+
+
+def cal(x):
+    return wr * x + br
 
 
 if __name__ == '__main__':
+    # data = np.array([[1, 2, 3], [4, 5, 6]]).T.reshape(3, 2)
+    # z = np.array([10, 100]).reshape((1, 2))
+    # print(np.sum(data*z, axis=1, keepdims=True)/2)
+    # data = pd.read_csv("../data/test_data.csv", dtype={'x1': np.float64, 'x2': np.float64, 'y': np.int32})
     data = pd.read_csv("../data/data.csv", dtype={'x1': np.float64, 'x2': np.float64, 'y': np.int32})
     m = len(data)
     nd_label = data['y'].values.reshape((1, m))
     nd_data = data.drop(columns='y').values.T
 
-    print(nd_label)
-    print(nd_data.shape)
+    no_features, _ = nd_data.shape
+    lg = LogisticRegression(no_features)
+    lg.set_iter(10000)
+    lg.fit(nd_data, nd_label)
+
+    ww = lg.w
+
+    wr = -ww[0, 0] / ww[1, 0]
+    br = -lg.b[0, 0] / ww[1, 0]
+    print(wr, br)
+
+    dv.pt(nd_data, nd_label, cal)
+
+    lg.print()
